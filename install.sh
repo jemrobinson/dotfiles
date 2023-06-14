5c4967e66a4875787ddc27191341acd27abc8f64
James Robinson <james.em.robinson@gmail.com>
2023-06-14 11:55:08 +0100

    :sparkles: Install executables with stow

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env sh
 
+# HOME_DIRECTORY="${HOME}"
+HOME_DIRECTORY="${HOME}/tmp"
 
 # Install Homebrew
 if ! (type brew > /dev/null 2>&1); then
@@ -12,4 +14,12 @@ homebrew/install.sh
 
 # Install dotfiles with stow
 echo "⚫ Installing dotfiles with stow"
-stow -d dotfiles -t .. -v -R $(ls dotfiles)
+for category in $(ls dotfiles); do
+    echo "  ⌛ Working on $category"
+    stow -d dotfiles -t "$HOME_DIRECTORY" -R "$category"
+done
+
+# Install executables with stow
+echo "❗ Installing executables with stow"
+mkdir -p "$HOME_DIRECTORY"/.local/bin
+stow -t "$HOME_DIRECTORY"/.local/bin -R executables
