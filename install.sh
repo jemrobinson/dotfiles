93c57bef24aa0f075dc4f8ac8493b4761bb27ed3
James Robinson <james.em.robinson@gmail.com>
2023-06-16 03:00:12 +0100

    :memo: Added fonts

--- install.sh
+++ install.sh
@@ -33,3 +33,10 @@ for executable in $(ls "${HOME_DIRECTORY}/.local/bin"); do echo "  ✅ Installed
 echo "🍺 Installing Homebrew packages"
 homebrew/install.sh
 
+# Print messages
+echo "Install the Powerline font by double-clicking the .ttf in the 'fonts' directory"
+echo "In case of problems, see here: https://gist.github.com/kevin-smets/8568070 and https://github.com/romkatv/powerlevel10k#meslo-nerd-font-patched-for-powerlevel10k"
+echo ""
+echo "Use the following colour-schemes in iTerm2"
+echo "* zsh should use 'tango-dark-tweaked' (import from the iterm directory)"
+echo "* Powershell should use 'shades-of-purple' (import from the iterm directory)"
