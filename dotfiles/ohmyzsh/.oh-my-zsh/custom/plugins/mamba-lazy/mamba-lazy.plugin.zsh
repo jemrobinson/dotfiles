0474228b7250fbf6c7a7782af6d1ed2fab3b21f2
James Robinson <james.em.robinson@gmail.com>
2025-07-18 12:16:38 +0100

    :art: Make lazy-mamba into a plugin

---
+++ dotfiles/ohmyzsh/.oh-my-zsh/custom/plugins/mamba-lazy/mamba-lazy.plugin.zsh
@@ -0,0 +1,23 @@
+# Lazy load mamba
+lazy_mamba_aliases=('mamba' 'micromamba')
+load_mamba() {
+  for lazy_mamba_alias in $lazy_mamba_aliases; do
+    unalias $lazy_mamba_alias
+  done
+
+  # Set up mamba
+  export MAMBA_EXE='/opt/homebrew/bin/micromamba';
+  export MAMBA_ROOT_PREFIX='/Users/jrobinson/.config/mamba';
+  __mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+  if [ $? -eq 0 ]; then
+      eval "$__mamba_setup"
+  else
+      alias mamba="$MAMBA_EXE"  # Fallback on help from mamba activate
+  fi
+  unset __mamba_setup
+  unfunction load_mamba
+}
+
+for lazy_mamba_alias in $lazy_mamba_aliases; do
+  alias $lazy_mamba_alias="load_mamba && $lazy_mamba_alias"
+done
